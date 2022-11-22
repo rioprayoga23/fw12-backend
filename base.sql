e00a87c2f97706e421c6b910c6cc86e439dcd5d5
Rio Prayoga <yoprayogap@gmail.com>
2022-11-22 14:25:53 +0700

    merubah nama kolom

--- base.sql
+++ base.sql
@@ -1,3 +1,4 @@
+-- Active: 1669095951493@@127.0.0.1@5432@tiketku@public
 -- Active: 1669025233004@@127.0.0.1@5432@tiketku@public
 -- Active: 1669010052102@@127.0.0.1@5432@go_nobar@public
 
@@ -7,11 +8,11 @@
 
 CREATE TABLE "users" (
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "image" VARCHAR(225),
+    "picture" VARCHAR(225),
     "firstName" VARCHAR(225),
     "lastName" VARCHAR(225),
     "phoneNumber" VARCHAR(225),
-    "email" VARCHAR(225),
+    "email" VARCHAR(225) UNIQUE,
     "password" VARCHAR(225),
     "isAdmin" BOOLEAN NULL DEFAULT false,
     "createdAt" TIMESTAMP DEFAULT now(),
@@ -20,7 +21,7 @@ CREATE TABLE "users" (
 
 CREATE TABLE "forgotPassword"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "idUser" INT,
+    "userId" INT,
     "code" VARCHAR(225),
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
@@ -29,7 +30,7 @@ CREATE TABLE "forgotPassword"(
 CREATE TABLE "movies"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     "title" VARCHAR(225),
-    "image" VARCHAR(225),
+    "picture" VARCHAR(225),
     "releaseDate" TIMESTAMP,
     "director" VARCHAR(225),
     "duration" TIME,
@@ -45,8 +46,8 @@ CREATE TABLE "genre"(
 );
 CREATE TABLE "movieGenre"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "idMovie" INT,
-    "idGenre" INT,
+    "movieId" INT,
+    "genreId" INT,
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
 );
@@ -58,14 +59,14 @@ CREATE TABLE "casts"(
 );
 CREATE TABLE "movieCast"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "idMovie" INT,
-    "idCast" INT,
+    "movieId" INT,
+    "castId" INT,
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
 );
 CREATE TABLE "cinemas"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "image" VARCHAR(225),
+    "picture" VARCHAR(225),
     "name" VARCHAR(225),
     "address" VARCHAR(225),
     "city" VARCHAR(225),
@@ -74,8 +75,8 @@ CREATE TABLE "cinemas"(
 );
 CREATE TABLE "movieSchedules"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "idMovie" INT,
-    "idCinema" INT,
+    "movieId" INT,
+    "cinemaId" INT,
     "price" BIGINT,
     "startDate" TIMESTAMP,
     "endDate" TIMESTAMP,
@@ -85,7 +86,7 @@ CREATE TABLE "movieSchedules"(
 CREATE TABLE "movieScheduleTimes"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     "time" TIME,
-    "idMovieSchedule" INT,
+    "movieScheduleId" INT,
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
 );
@@ -98,9 +99,9 @@ CREATE TABLE "status"(
 CREATE TABLE "transactions"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     "bookingDate" TIMESTAMP,
-    "idMovie" INT,
-    "idCinema" INT,
-    "idMovieSchedule" INT,
+    "movieId" INT,
+    "cinemaId" INT,
+    "movieScheduleId" INT,
     "fullName" VARCHAR(225),
     "email" VARCHAR(225),
     "phoneNumber" VARCHAR(225),
@@ -111,14 +112,14 @@ CREATE TABLE "transactions"(
 CREATE TABLE "reservedSeat"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     "seatNum" VARCHAR(225),
-    "idTransaction" INT,
+    "transactionId" INT,
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
 );
 
 CREATE TABLE "paymentMethod"(
     "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    "image" VARCHAR(225),
+    "picture" VARCHAR(225),
     "name" VARCHAR(225),
     "createdAt" TIMESTAMP DEFAULT now(),
     "updatedAt" TIMESTAMP 
@@ -135,10 +136,10 @@ CREATE TABLE "subscribers"(
 -- * users
 SELECT * FROM users;
 
-INSERT INTO users (image,"firstName","lastName","phoneNumber",email,password,"isAdmin") 
-VALUES ('https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg','admin','ganteng','089890890890','admin@gmail.com','admin123',true);
+INSERT INTO users (picture,"firstName","lastName","phoneNumber",email,password,"isAdmin") 
+VALUES ('https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg','super','admin','089890890890','admin@gmail.com','admin123',true);
 
-INSERT INTO users (image,"firstName","lastName","phoneNumber",email,"password") 
+INSERT INTO users (picture,"firstName","lastName","phoneNumber",email,"password") 
 VALUES ('https://static.vecteezy.com/system/resources/previews/002/318/271/original/user-profile-icon-free-vector.jpg','rio','prayoga','085785667890','rio@gmail.com','rio123');
 
 -- * end users
@@ -146,18 +147,18 @@ VALUES ('https://static.vecteezy.com/system/resources/previews/002/318/271/origi
 -- *forgotPassword
 SELECT * FROM "forgotPassword";
 
-INSERT INTO "forgotPassword" ("idUser",code)
+INSERT INTO "forgotPassword" ("userId",code)
 VALUES (2,'45678');
 
-INSERT INTO "forgotPassword" ("idUser",code)
+INSERT INTO "forgotPassword" ("userId",code)
 VALUES (2,'56789');
 
 -- *end forgotPassword
 
 -- *movies
 SELECT * FROM movies;
-INSERT INTO movies (title,image,"releaseDate",director,duration,synopsis)
-VALUES ('Spiderman','https:://spiderman.jpg','2018-01-01 00:00:01','jhon doe','02:15:00','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, placeat ratione. Aut eveniet, sapiente iusto odio nesciunt odit magni ducimus');
+INSERT INTO movies (title,picture,"releaseDate",director,duration,synopsis)
+VALUES ('Spiderman','https:://spiderman.jpg','1669100607','jhon doe','02:15:00','Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, placeat ratione. Aut eveniet, sapiente iusto odio nesciunt odit magni ducimus');
 -- *end movies
 
 -- *cast
@@ -171,8 +172,8 @@ INSERT INTO casts (name) VALUES ('Joe Taslim');
 -- *movie cast
 SELECT * FROM "movieCast";
 
-INSERT INTO "movieCast" ("idMovie","idCast") VALUES (1,1);
-INSERT INTO "movieCast" ("idMovie","idCast") VALUES (1,2);
+INSERT INTO "movieCast" ("movieId","castId") VALUES (1,1);
+INSERT INTO "movieCast" ("movieId","castId") VALUES (1,2);
 -- *end movie cast
 
 -- * genre
@@ -186,30 +187,29 @@ INSERT INTO genre(name) VALUES ('adventure');
 -- *movieGenre
 SELECT * FROM "movieGenre";
 
-INSERT INTO "movieGenre" ("idMovie","idGenre") VALUES (1,1);
-INSERT INTO "movieGenre" ("idMovie","idGenre") VALUES (1,2);
+INSERT INTO "movieGenre" ("movieId","genreId") VALUES (1,1);
+INSERT INTO "movieGenre" ("movieId","genreId") VALUES (1,2);
 
 -- *end movieGenre
 
 -- *cinemas
 SELECT * FROM cinemas;
 
-INSERT INTO cinemas(image,name,address,city) 
+INSERT INTO cinemas(picture,name,address,city) 
 VALUES ('https:://cinema21.jpg', 'Cinema 21', 'Jawa Barat','Bandung');
 
 -- *end cinemas
 
 -- *movieSchedule
 SELECT * FROM "movieSchedules";
-INSERT INTO "movieSchedules" ("idMovie","idCinema",price,"startDate","endDate") 
-VALUES (1,1,35000,'2018-01-01 00:00:01','2018-03-01 00:00:01');
+INSERT INTO "movieSchedules" ("movieId","cinemaId",price,"startDate","endDate") 
+VALUES (1,1,35000,'1669100607','1674371007');
 -- *end movieSchedule
 
 -- *movieScheduleTimes
-
 SELECT * FROM "movieScheduleTimes";
 
-INSERT INTO "movieScheduleTimes"(time,"idMovieSchedule") 
+INSERT INTO "movieScheduleTimes"(time,"movieScheduleId") 
 VALUES ('14:00:00',1);
 
 -- *end movieScheduleTimes
@@ -224,19 +224,19 @@ INSERT INTO status(name) VALUES ('active');
 -- *transactions
 SELECT * FROM transactions;
 
-INSERT INTO transactions ("bookingDate","idMovie","idCinema","idMovieSchedule","fullName",email,"phoneNumber","idStatus") VALUES ('2018-01-01 00:00:01',1,1,1,'rio prayoga','rio@gmail.com','089789765789',1);
+INSERT INTO transactions ("bookingDate","movieId","cinemaId","movieScheduleId","fullName",email,"phoneNumber","idStatus") VALUES ('1674371007',1,1,1,'rio prayoga','rio@gmail.com','089789765789',1);
 
 -- *end transactions
 
 --* resevedSeat 
 SELECT * FROM "reservedSeat";
-INSERT INTO "reservedSeat"("seatNum","idTransaction") VALUES ('A1',1);
+INSERT INTO "reservedSeat"("seatNum","transactionId") VALUES ('A1',1);
 --*end resevedSeat 
 
 --*paymentMethod
 SELECT * FROM "paymentMethod";
 
-INSERT INTO "paymentMethod" (image,name) VALUES ('gopay.jpg','gopay');
+INSERT INTO "paymentMethod" (picture,name) VALUES ('gopay.jpg','gopay');
 --*end paymentMethod
 
 --* subscibers 
