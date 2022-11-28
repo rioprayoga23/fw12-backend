15a796bcabc5948335834b124b08d2772487d297
Rio Prayoga <yoprayogap@gmail.com>
2022-11-28 13:24:39 +0700

    handling error duplicate

--- src/helpers/errorHandler.helper.js
+++ src/helpers/errorHandler.helper.js
@@ -5,8 +5,27 @@ exports.errorHandler = (error, res) => {
       message: "Email already used",
     });
   }
-  return res.status(500).json({
-    success: false,
-    message: `Something error in our backend : ${error.message}`,
-  });
+  if (error.message.includes(' unique constraint "UniqueNameStatus"')) {
+    return res.status(400).json({
+      success: false,
+      message: "Status Name already",
+    });
+  }
+  if (error.message.includes(' unique constraint "UniqueEmailSubscribers"')) {
+    return res.status(400).json({
+      success: false,
+      message: "Email already used",
+    });
+  }
+  if (error.message.includes(' unique constraint "UniqueNameGenre"')) {
+    return res.status(400).json({
+      success: false,
+      message: "Genre already",
+    });
+  }
+  if (error.message.includes())
+    return res.status(500).json({
+      success: false,
+      message: `Something error in our backend : ${error.message}`,
+    });
 };
