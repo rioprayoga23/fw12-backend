0a6ca997b2d61318d2a8ba1073f35fe3857ffc9d
Rio Prayoga <yoprayogap@gmail.com>
2022-12-06 22:20:18 +0700

    routing folder public

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/assets/uploads", express.static("uploads/"));
+app.use("/uploads", express.static("uploads/"));
 
 app.use("/", require("./src/routes"));
 
