57505e82ed3820c7ae27031606f59dbc798c20f7
Rio Prayoga <yoprayogap@gmail.com>
2022-12-11 19:33:09 +0700

    mengubah koneksi ke lokal

--- src/helpers/db.helper.js
+++ src/helpers/db.helper.js
@@ -1,8 +1,7 @@
 const { Pool } = require("pg");
 
 const db = new Pool({
-  connectionString:
-    "postgres://postgres:KTAm64vsBJjfSzDQ@db.nviuugjfkaynwjepxmoy.supabase.co:6543/postgres",
+  connectionString: "postgresql:postgres:1@localhost:5432/tiketku",
 });
 
 db.connect((err) => {
