3ec7f97c5e611ca7016abcafb5f85d9ad2a33909
Rio Prayoga <yoprayogap@gmail.com>
2022-12-13 11:48:20 +0700

    -

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -47,7 +47,7 @@ exports.createUser = (req, res) => {
   if (errorValidation) {
     return res.status(400).json({
       success: false,
-      message: errorValidation,
+      message: errorValidation.errors.msg,
     });
   }
   createUser(req.body, (error, results) => {
