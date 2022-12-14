e47992ce424ef89d4864e562189eefa7310e42a5
Rio Prayoga <yoprayogap@gmail.com>
2022-12-14 19:42:47 +0700

    mengubah path uploads

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/uploads", express.static("uploads/"));
+app.use(
+  "/uploads",
+  express.static(require("path").join(process.cwd(), "uploads/"))
+);
 
 app.use("/", require("./src/routes"));
 
