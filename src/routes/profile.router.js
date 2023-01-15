547d74c978afb5e802612b51f266a18731ad0ee9
Rio Prayoga <yoprayogap@gmail.com>
2023-01-15 16:02:09 +0700

    add validation reset password in update profile

--- src/routes/profile.router.js
+++ src/routes/profile.router.js
@@ -4,10 +4,17 @@ const {
   readProfile,
 } = require("../controllers/profile.controller");
 const uploadMiddleware = require("../middleware/upload.middleware");
+const { rules, validate } = require("../middleware/validator.middleware");
 
 const userRouter = require("express").Router();
 
 userRouter.get("/", readProfile);
-userRouter.patch("/", uploadMiddleware, updateProfile);
+userRouter.patch(
+  "/",
+  uploadMiddleware,
+  rules("resetPassword"),
+  validate,
+  updateProfile
+);
 
 module.exports = userRouter;
