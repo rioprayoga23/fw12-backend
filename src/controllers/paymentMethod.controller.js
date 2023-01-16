29f000308c190264b26d97544b07fe3be762e6c6
Rio Prayoga <yoprayogap@gmail.com>
2023-01-16 19:09:19 +0700

    fix response key

--- src/controllers/paymentMethod.controller.js
+++ src/controllers/paymentMethod.controller.js
@@ -14,7 +14,7 @@ exports.readAllPayments = (req, res) => {
     }
     return res.status(200).json({
       success: true,
-      result: results.rows,
+      results: results.rows,
     });
   });
 };
