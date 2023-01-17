00f6ac1d5367bf2d26cd9896d610a9a27be2dcab
Rio Prayoga <yoprayogap@gmail.com>
2023-01-17 21:07:06 +0700

    dev-trx-2.0

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -129,18 +129,18 @@ exports.createOrder = async (data, userId, callback) => {
       dataBody.bookingTime,
     ]);
 
+    let rsvQuery;
     const seatNumArr = Object.keys(dataBody.seatNum);
     seatNumArr.forEach(async (item, index) => {
       const sqlReservedSeat = `INSERT INTO "reservedSeat" ("seatNum","transactionId") VALUES ($1,currval(pg_get_serial_sequence('transactions','id'))) RETURNING *`;
 
-      const rsvQuery = await db.query(sqlReservedSeat, [item]);
+      rsvQuery = await db.query(sqlReservedSeat, [item]);
     });
-
     await db.query("COMMIT");
 
     const results = {
       trxQuery,
-      // rsvQuery,
+      rsvQuery,
     };
 
     callback(null, results);
