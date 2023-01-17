2bfa3cdfcd07c5a233fa0e8b0bddc1f51da3340d
Rio Prayoga <yoprayogap@gmail.com>
2023-01-17 20:20:55 +0700

    add total in response ticket

--- src/models/ticketResult.model.js
+++ src/models/ticketResult.model.js
@@ -1,7 +1,7 @@
 const db = require("../helpers/db.helper");
 
 exports.readTicketResult = (id, callback) => {
-  const sql = `SELECT m.title, string_agg(distinct g.name,', ') AS genre,t."bookingDate", t."bookingTime",COUNT(distinct rs."seatNum") AS count,string_agg(distinct rs."seatNum",', ') AS "seatNum" FROM transactions t
+  const sql = `SELECT m.title, string_agg(distinct g.name,', ') AS genre,t."bookingDate", t."bookingTime",COUNT(distinct rs."seatNum") AS count,string_agg(distinct rs."seatNum",', ') AS "seatNum", t.total FROM transactions t
   JOIN movies m ON m.id = t."movieId"
   JOIN "movieGenre" mg ON mg."movieId" = m.id
   JOIN genre g ON g.id = mg."genreId"
